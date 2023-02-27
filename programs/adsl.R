0dbf07e095838fab4d77e85f51da9353a3def4d8
bimalthomas1997 <bimal.thomas@genproresearch.com>
2023-02-27 05:37:10 +0000

    Initial commit

--- programs/adsl.R
+++ programs/adsl.R
@@ -23,33 +23,395 @@ sc <- read_xpt("sdtm/sc.xpt")
 sv <- read_xpt("sdtm/sv.xpt")
 mh <- read_xpt("sdtm/mh.xpt")
 vs <- read_xpt("sdtm/vs.xpt")
+qs <- read_xpt("sdtm/qs.xpt")
 
 metacore <- spec_to_metacore("metadata/specs.xlsx", where_sep_sheet = FALSE)
 
 adsl_spec <- metacore %>%
   select_dataset("ADSL")
 
+dm <- convert_blanks_to_na(dm)
+
 # Creating TRT01P/TRT01A/AGEGR1
-adsl_1 <- dm %>%
+adsl <- dm %>%
   filter(ARMCD!="Scrnfail") %>%
   mutate(TRT01P=ARM,
-         TRT01A=TRT01P,
-         AGEGR1N=ifelse(AGE<65,1,ifelse(AGE>=65&AGE<=80,2,ifelse(AGE>80,3,NA))),
-         AGEGR1=ifelse(AGEGR1N==1,"<65",ifelse(AGEGR1N==2,"65-80",ifelse(AGEGR1N==3,">80",NA_character_)))) %>%
+         TRT01A=TRT01P) %>%
   derive_vars_dtm(dtc = RFSTDTC,
                   new_vars_prefix = "TRTS") %>%
   derive_vars_dtm(dtc = RFENDTC,
                   new_vars_prefix = "TRTE") %>%
   derive_vars_dtm_to_dt(source_vars = vars(TRTSDTM, TRTEDTM)) %>%
-
-
-  pooled_sites <- dm %>%
-  filter(ARMCD != "Scrnfail") %>%
   group_by(SITEID, ARM) %>%
-  summarise(grp_size = n(),.groups =NULL ) %>%
-  filter (grp_size < 3) %>%
-  select (SITEID,grp_size,ARM) %>%
-  distinct()
+  mutate(size=n(),
+         SITEGR1=ifelse(size<3,"900",SITEID)) %>%
+  ungroup() %>%
+  mutate(ITTFL=ifelse(ARMCD!='',"Y","N"),
+         SAFFL=ifelse(ITTFL=="Y"&!is.na(TRTSDT),"Y","N"))
+
+# HEIGHTBL
+adsl <- adsl %>%  derive_vars_merged( dataset_add = vs,
+                                    by_vars = vars(STUDYID,USUBJID),
+                                    filter_add = (VSTESTCD=='HEIGHT' & VISITNUM == 1),
+                                    new_vars = vars(HEIGHTBL = VSSTRESN))
+
+# WEIGHTBL
+adsl <- adsl %>%  derive_vars_merged( dataset_add = vs,
+                                    by_vars = vars(STUDYID,USUBJID),
+                                    filter_add = (VSTESTCD=='WEIGHT' & VISITNUM == 3),
+                                    new_vars = vars(WEIGHTBL = VSSTRESN))
+
+# BMIBL
+adsl <- adsl %>%  mutate(BMIBL = compute_bmi(HEIGHTBL, WEIGHTBL))
+
+# BMIBLGR1/AGEGR1
+adsl <- adsl %>%  create_cat_var(adsl_spec, BMIBL, BMIBLGR1) %>%
+  create_cat_var(adsl_spec, AGE, AGEGR1)
+
+# for COMP16FL
+sv_16 <- sv %>%
+  filter(VISITNUM==10) %>%
+  select(STUDYID,USUBJID,SVSTDTC) %>%
+  rename(COMP16D=SVSTDTC)
+
+# for COMP24FL
+sv_24 <- sv %>%
+  filter(VISITNUM==12) %>%
+  select(STUDYID,USUBJID,SVSTDTC) %>%
+  rename(COMP24D=SVSTDTC)
+
+# for COMP8FL
+sv_8 <- sv %>%
+  filter(VISITNUM==8) %>%
+  select(STUDYID,USUBJID,SVSTDTC) %>%
+  rename(COMP8D=SVSTDTC)
+
+# COMP16FL/COMP24FL/COMP8FL
+adsl <- adsl %>% mutate(DATE1=as.POSIXct(RFSTDTC,format="%Y-%m-%dT%H:%M",tz="UTC")) %>%
+  derive_vars_merged(dataset_add =sv_16,
+                     by_vars = vars(STUDYID, USUBJID)) %>%
+  mutate(DATE2=as.POSIXct(COMP16D,format="%Y-%m-%dT%H:%M",tz="UTC"),
+         COMP16FL=ifelse(DATE1>=DATE2),"Y","N") %>%
+  derive_vars_merged(dataset_add =sv_24,
+                     by_vars = vars(STUDYID, USUBJID)) %>%
+  mutate(DATE2=as.POSIXct(COMP24D,format="%Y-%m-%dT%H:%M",tz="UTC"),
+         COMP24FL=ifelse(DATE1>=DATE2),"Y","N") %>%
+  derive_vars_merged(dataset_add =sv_8,
+                     by_vars = vars(STUDYID, USUBJID)) %>%
+  mutate(DATE2=as.POSIXct(COMP8D,format="%Y-%m-%dT%H:%M",tz="UTC"),
+         COMP8FL=ifelse(DATE1>=DATE2),"Y","N")
+
+# VISIT1DT
+adsl <- adsl %>%  derive_vars_merged(dataset_add = sv,
+                                     by_vars = vars(STUDYID, USUBJID),
+                                     filter_add = VISITNUM == 1,
+                                     new_vars = vars(VISIT1DT = SVSTDTC))
+
+# # DCDECODE
+# adsl <- adsl %>%  derive_vars_merged( dataset_add = ds,
+#                                     by_vars = vars(STUDYID,USUBJID),
+#                                     filter_add = DSCAT=='DISPOSITION EVENT',
+#                                     new_vars = vars(DCDECOD = DSDECOD))
+
+# EDUCLVL
+adsl <- adsl %>%  derive_vars_merged(dataset_add = sc,
+                                   by_vars = vars(STUDYID, USUBJID),
+                                   filter_add = SCTESTCD == 'EDLEVEL',
+                                   new_vars = vars(EDUCLVL = SCSTRESN))
+
+# RACEN/AGEGR1N/TRT01AN/TRT01PN
+adsl <- adsl %>% create_var_from_codelist(adsl_spec,RACE, RACEN) %>%
+  create_var_from_codelist(adsl_spec,AGEGR1, AGEGR1N) %>%
+  create_var_from_codelist(adsl_spec,TRT01P, TRT01PN) %>%
+  create_var_from_codelist(adsl_spec,TRT01A, TRT01AN)
+
+# Deriving VISNUMEN from ds
+
+ds <- ds %>%
+  mutate(VISNUMEN = case_when(VISITNUM == 13 & DSTERM == "PROTOCOL COMPLETED" ~ 12,
+                              TRUE ~ VISITNUM))
+
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = ds,
+    filter_add = (DSCAT == "DISPOSITION EVENT"),
+    new_vars = vars(DCDECOD = DSDECOD, VISNUMEN, DCREASCD = DSTERM),
+    order = vars(DSDECOD, VISNUMEN, DSTERM),
+    mode = "first",
+    by_vars = vars(STUDYID, USUBJID)
+  )
+
+# EOSSTT
+format_eosstt <- function(DSDECOD) {
+  case_when(DSDECOD %in% c("COMPLETED") ~ "COMPLETED",
+            DSDECOD %in% c("SCREEN FAILURE") ~ NA_character_,
+            !is.na(DSDECOD) ~ "DISCONTINUED",
+            TRUE ~ "ONGOING")
+}
+
+adsl <- adsl %>%
+  derive_var_disposition_status(dataset_ds = ds,
+                                new_var = EOSSTT,
+                                status_var = DSDECOD,
+                                format_new_var = format_eosstt,
+                                filter_ds = DSCAT == "DISPOSITION EVENT")
+
+# DISONSDT
+adsl <- adsl %>%
+  derive_vars_merged(dataset_add = mh,
+                     filter_add = (MHCAT == "PRIMARY DIAGNOSIS"),
+                     new_vars = vars(DISONSDT = MHSTDTC),
+                     order = vars(MHSTDTC),
+                     mode = 'first',
+                     by_vars = vars(STUDYID, USUBJID))
+
+# MSSETOT
+
+qs <- qs %>%
+  select(USUBJID, QSORRES, QSCAT, VISITNUM)
+
+USUBJID = vector("character", 0)
+MMSETOT = vector("integer", 0)
+flag_ADASCog = vector("character", 0)
+flag_CIBIC = vector("character", 0)
+
+n_MMSETOT = 0
+
+is_ADASCog = FALSE
+is_CIBIC = FALSE
+
+for(i in 1:nrow(qs))
+{
+  if(i == 1)
+  {
+    subject = qs[1, 1]
+    USUBJID = c(subject)
+  }
+
+  if(qs[i, 3] == "MINI-MENTAL STATE")
+  {
+    n_MMSETOT = n_MMSETOT + as.integer(qs[i, 2])
+  }
+
+  if(qs[i, 3] == "ALZHEIMER'S DISEASE ASSESSMENT SCALE" & qs[i, 4] > 3 & is_ADASCog == FALSE)
+  {
+    flag_ADASCog = c(flag_ADASCog, 'Y')
+    is_ADASCog = TRUE
+  }
+
+  if(qs[i, 3] == "CLINICIAN'S INTERVIEW-BASED IMPRESSION OF CHANGE (CIBIC+)" & qs[i, 4] > 3 & is_CIBIC == FALSE)
+  {
+    flag_CIBIC = c(flag_CIBIC, 'Y')
+    is_CIBIC = TRUE
+  }
+
+  if(qs[i, 1] != subject)
+  {
+    MMSETOT = c(MMSETOT, n_MMSETOT)
+    subject = qs[i, 1]
+    USUBJID = c(USUBJID, subject)
+    n_MMSETOT = 0
+
+    if(is_ADASCog == TRUE)
+    {
+      is_ADASCog = FALSE
+    }
+    else
+    {
+      flag_ADASCog = c(flag_ADASCog, 'N')
+    }
+
+    if(is_CIBIC == TRUE)
+    {
+      is_CBIC = FALSE
+    }
+    else
+    {
+      flag_CIBIC = c(flag_CIBIC, 'N')
+    }
+  }
+}
+
+MMSETOT = c(MMSETOT, n_MMSETOT)
+
+if(is_ADASCog == TRUE)
+{
+  is_ADASCog = FALSE
+} else
+{
+  flag_ADASCog = c(flag_ADASCog, 'N')
+}
+
+if(is_CIBIC == TRUE)
+{
+  is_CBIC = FALSE
+} else
+{
+  flag_CIBIC = c(flag_CIBIC, 'N')
+}
+
+USUBJID <- paste(USUBJID)
+MMSETOT <- paste(MMSETOT)
+
+qs_adsl <- cbind(USUBJID, MMSETOT, flag_ADASCog, flag_CIBIC)
+
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = as.data.frame(qs_adsl),
+    new_vars = vars(MMSETOT, flag_ADASCog, flag_CIBIC),
+    order = vars(MMSETOT),
+    mode = 'first',
+    by_vars = vars(USUBJID)) %>% # Deriving VISIT4DT from sv to derive CUMDOSE
+derive_vars_merged(
+  dataset_add = sv,
+  filter_add = (VISITNUM == 4),
+  new_vars = vars(VISIT4DT = SVSTDTC),
+  order = vars(SVSTDTC),
+  mode = 'first',
+  by_vars = vars(STUDYID, USUBJID)) %>% # Deriving VISIT12DT from sv to derive CUMDOSE
+  derive_vars_merged(
+    dataset_add = sv,
+    filter_add = (VISITNUM == 12),
+    new_vars = vars(VISIT12DT = SVSTDTC),
+    order = vars(SVSTDTC),
+    mode = 'first',
+    by_vars = vars(STUDYID, USUBJID)) %>% # Deriving DURDIS
+  mutate(VISIT1DT = as.Date(VISIT1DT),
+         VISIT4DT = as.Date(VISIT4DT),
+         VISIT12DT = as.Date(VISIT12DT),
+         DISONSDT = as.Date(DISONSDT),
+         TRTSDT = as.Date(TRTSDT),
+         TRTEDT = as.Date(TRTEDT))
+
+adsl$DURDIS <- compute_duration(
+  adsl$DISONSDT,
+  adsl$VISIT1DT,
+  in_unit = "days",
+  out_unit = "months",
+  floor_in = TRUE,
+  add_one = TRUE,
+  trunc_out = FALSE
+)
+
+
+adsl$TRTDURD <- compute_duration(
+  adsl$TRTSDT,
+  adsl$TRTEDT,
+  in_unit = "days",
+  out_unit = "days",
+  floor_in = TRUE,
+  add_one = TRUE,
+  trunc_out = FALSE
+)
+
+# Deriving variables used for caluculating CUMDOSE
+
+adsl$Interval_1 <- compute_duration(
+  adsl$TRTSDT,
+  adsl$VISIT4DT,
+  in_unit = "days",
+  out_unit = "days",
+  floor_in = TRUE,
+  add_one = TRUE,
+  trunc_out = FALSE
+)
+
+adsl$Interval_1_Discontinued <- compute_duration(
+  adsl$TRTSDT,
+  adsl$TRTEDT,
+  in_unit = "days",
+  out_unit = "days",
+  floor_in = TRUE,
+  add_one = TRUE,
+  trunc_out = FALSE
+)
+
+adsl$Interval_2 <- compute_duration(
+  adsl$VISIT4DT,
+  adsl$VISIT12DT,
+  in_unit = "days",
+  out_unit = "days",
+  floor_in = TRUE,
+  add_one = FALSE,
+  trunc_out = FALSE
+)
+
+adsl$Interval_3 <- compute_duration(
+  adsl$VISIT12DT,
+  adsl$TRTEDT,
+  in_unit = "days",
+  out_unit = "days",
+  floor_in = TRUE,
+  add_one = FALSE,
+  trunc_out = FALSE
+)
+
+adsl$Interval_2_Discontinued <- compute_duration(
+  adsl$VISIT4DT,
+  adsl$TRTEDT,
+  in_unit = "days",
+  out_unit = "days",
+  floor_in = TRUE,
+  add_one = FALSE,
+  trunc_out = FALSE
+)
+
+# DCSREAS
+adsl <- adsl %>%
+  derive_vars_disposition_reason(
+    dataset_ds = ds,
+    new_var = DCSREAS,
+    reason_var = DSDECOD,
+    new_var_spe = DCSREASP,
+    reason_var_spe = DSTERM,
+    filter_ds = DSCAT == "DISPOSITION EVENT" & DSDECOD != "SCREEN FAILURE"
+  )
+
+adsl <- adsl %>%
+  mutate( ARMN = case_when(ARM == "Placebo" ~ 0,
+                           ARM == "Xanomeline Low Dose" ~ 1,
+                           ARM == "Xanomeline High Dose" ~ 2),
+          CUMDOSE = case_when(ARMN == 0 ~ 0,
+                             ARMN == 1 ~ TRT01PN * TRTDURD,
+                             ARMN == 2 ~ case_when(VISNUMEN > 3 & VISNUMEN <= 4 ~ case_when(EOSSTT == "COMPLETED" ~ 54 * Interval_1,
+                                                                                            EOSSTT == "DISCONTINUED" ~ 54 * Interval_1_Discontinued),
+                                                   VISNUMEN > 4 & VISNUMEN <= 12 ~ case_when(EOSSTT == "COMPLETED" ~ 54 * Interval_1 + 81 * Interval_2 + 54 * Interval_3,
+                                                                                             EOSSTT == "DISCONTINUED" ~ 54 * Interval_1 + 81 * Interval_2_Discontinued),
+                                                   VISNUMEN > 12 ~ 54 * Interval_1 + 81 * Interval_2 + 54 * Interval_3)),
+         AVGDD = CUMDOSE / TRTDURD,
+         DISCONFL = case_when(DCREASCD == "PROTOCOL COMPLETED" ~ 'Y'),
+         DSRAEFL = case_when(DCREASCD == "ADVERSE EVENT" ~ 'Y'),
+         DURDSGR1 = case_when(DURDIS < 12 ~ "<12",
+                              DURDIS >= 12 ~ ">=12"),
+         EFFFL = case_when(SAFFL == 'Y' & flag_ADASCog == 'Y' & flag_CIBIC == 'Y' ~ 'Y',
+                           TRUE ~ 'N'),
+         RFENDT = format(as.Date(RFENDTC), "%d-%b-%Y"),
+         RFSTDTC = format(as.Date(RFSTDTC), "%d-%b-%Y"),
+         RFENDTC = format(as.Date(RFENDTC), "%d-%b-%Y"),
+         TRTSDT = format(as.Date(TRTSDT), "%d-%b-%Y"),
+         TRTEDT = format(as.Date(TRTEDT), "%d-%b-%Y"),
+         VISIT1DT = format(as.Date(VISIT1DT), "%d-%b-%Y"),
+         VISIT4DT = format(as.Date(VISIT4DT), "%d-%b-%Y"),
+         VISIT12DT = format(as.Date(VISIT12DT), "%d-%b-%Y"),
+         DISONSDT = format(as.Date(DISONSDT), "%d-%b-%Y"))
+
+
+# Adding labels and selecting required variables from metadata
+adsl<-adsl %>%
+  # only keep vars from define
+  drop_unspec_vars(adsl_spec) %>%
+  # order columns based on define
+  order_cols(adsl_spec) %>%
+  # apply variable labels based on define
+  set_variable_labels(adsl_spec) %>%
+  # Creating .XPT and adding dataset Label
+  xportr_write("adam/adsl.xpt",label = "Subject-Level Analysis Dataset")
+
+names(adsl)
+
+
+
+
 
 
 
